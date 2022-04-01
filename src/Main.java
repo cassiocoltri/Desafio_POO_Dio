a8dc550deb7430579165a33ff4765c6f7b6acd77
cassiocoltri <cassiocoltri@gmail.com>
2022-04-01 18:00:14 -0300

    atualização

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import dr.dio.desafio.dominio.Curso;
+import dr.dio.desafio.dominio.Mentoria;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+        Curso curso1 = new Curso("Curso de Java", "Como programar em Java", 10);
+        Curso curso2 = new Curso("Curso de Ecma", "Como programar em JavaScript", 5);
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria("Mentoria de Java", "Como decrarar as variáveis", LocalDate.now());
+
+        System.out.println(mentoria1);
 
     }
 }
